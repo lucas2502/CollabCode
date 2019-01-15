60193b41551ff4712746745100b8cda269b9c1f3
lucas2502 <lucas2502@live.com>
2019-01-15 14:26:44 +0000

    challenge 16

--- desafio-site/js/description-product.js
+++ desafio-site/js/description-product.js
@@ -1,21 +1,51 @@
-(function(){
-    'use strict';
-
     const $heart = document.querySelector(".-heart");
+    const $stars= document.querySelectorAll(".star");
+    const positionLast = $stars.length - 1;
 
-    $heart.addEventListener("click", handleClick);
-    console.log($heart);
-    
-    function handleClick() { 
-        /*if($heart.classList.contains("-active")){
-            $heart.classList.remove('-active');
+    $heart.addEventListener("click", handlelClick);
+
+    $stars.forEach(function($star, $key){
+        if(key ==0){
+            $star.addEventListener("click", firstStar);
         }
+
+        if(key == positionLast){
+            $star.addEventListener("click", lastStar);
+        }
+
         else{
-            $heart.classList.add('-active');
-        }*/
+            $star.addEventListener("click", function(){
+                middleStar(ket);
+            });
+        }
+    });
 
-        $heart.classList.toggle("-active");
-    };
+    function middleStar(index){
+        $stars.forEach(function($star, key){
+            $star.classList.remove("-active");
 
-})();
+            if( key <= index){
+                $star.classList.add("-active");
+            }
+        });
+    }
+
+    function firstStar(){
+        $stars.forEach(function($star){
+            $star.classList.remove("-active");
+        });
+
+        this.classList.add("-active");
+    }
+
+    function lastStar(){
+        $stars.forEach(function($star){
+            $star.classList.add("-active");
+        });
+    }
+
+    function handlelClick(){
+        this.classList.toggle("-active");
+    }
+    
 
